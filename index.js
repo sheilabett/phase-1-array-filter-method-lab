42e53b7cb2c67119ea3dc73b8c35eb09464cb5fd
sheilabett <sheilahbett@gmail.com>
2023-09-28 01:30:56 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+
+
+  function matchName(driverObjects, query) {
+    return driverObjects.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
+
+  const driverObjects = [
+  ];
+
+  console.log(findMatching);
+  console.log(fuzzyMatch); 
+  console.log(matchName);
